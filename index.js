ee2da7597eda8bf6af1352958214c33ac93edb08
canguruhh <sven@mutzl.org>
2020-03-21 18:44:39 +0100

    Ticket creation with evaluated answers prepared

--- index.js
+++ index.js
@@ -13,34 +13,42 @@ app.use(express.json());
 app.use(jsonErrorHandler());
 
 zammad.listUsers()
-.then(console.log)
 	.then(() => console.log('zammad works'));
 
 app.get('/', (req, res) => {
 	res.json({});
 });
-
 app.post('/ticket', async (req, res) => {
 
 	try {
 		let user = req.body.meta
+
+
+		//TODO: validate answers
+		let answers = req.body.answers
+
 		assert.ok(user, 'meta not present')
 		assert.ok(user.phone, 'phone not present')
 		user = await zammad.createUser(user)
-		eval.evaluate();
+
+		const { medical_priority, note } = await eval.evaluate(answers)
+
 		const response = await zammad.createTicket({
-			"title": "Ich brauche einen Test",
+			"title": "Corona Test",
 			"group": "Users",
 			"customer_id": user.id,
 			"article": {
-				"subject": "some subject",
-				"body": "some message",
+				"subject": "Corona Fragen",
+				"body": note,
 				"type": "note",
 				"internal": false
 			},
-			"note": "some note",
+			med_prio: medical_priority,
 		})
-		res.json(response)
+		res.json({
+			id: response.id,
+			priority: response.priority_id
+		})
 	} catch (error) {
 		console.error(error)
 		res.status(500).send(error.message)
