52ec47d656b16e426a4b098de1269b1861e6081b
Marvin Gaube <dev@marvingaube.de>
2020-03-21 13:24:49 +0100

    Add JSON Error handle

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const morgan = require('morgan');
+const jsonErrorHandler = require('express-json-error-handler').default;
 
 const app = express();
 app.use(express.json());
+app.use(jsonErrorHandler());
 
 app.get('/', (req,res) => {
 	res.json({});
 });
+
 app.listen(3000, () => {
 	console.debug('App listening on :3000');
 });
