67abe9651e755e9e4eaaff05b7dc675a92e58b1e
canguruhh <sven@mutzl.org>
2020-03-21 14:49:52 +0100

    Working example to create ticket

--- index.js
+++ index.js
@@ -9,11 +9,37 @@ const app = express();
 app.use(express.json());
 app.use(jsonErrorHandler());
 
-zammad.test();
+zammad.listTickets()
+	.then(() => console.log('zammad works'));
 
-app.get('/', (req,res) => {
+app.get('/', (req, res) => {
 	res.json({});
 });
+
+app.post('/ticket', async (req, res) => {
+	try {
+		const user = await zammad.createUser({
+			"firstname": "Bob",
+			"lastname": "Smith",
+		})
+		const response = await zammad.createTicket({
+			"title": "Ich brauche einen Test",
+			"group": "Users",
+			"customer_id": user.id,
+			"article": {
+				"subject": "some subject",
+				"body": "some message",
+				"type": "note",
+				"internal": false
+			},
+			"note": "some note",
+		})
+		res.json(response)
+	} catch (error) {
+		console.error(error)
+		res.status(500).send(error.message)
+	}
+})
 
 app.listen(3000, () => {
 	console.debug('App listening on :3000');
--- zammad.js
+++ zammad.js
@@ -1,23 +1,35 @@
-const Request = require('request');
+const superagent = require('superagent');
 
 class Zammad {
 	constructor() {
 		this.api_url = process.env.ZAMMAD_API_PATH || "https://zammad.example.com/api/";
 		this.api_token = process.env.ZAMMAD_API_TOKEN || "exampletoken";
 		console.debug("Connecting to zammad at "+this.api_url);
-		this.request = Request.defaults({
-			baseUrl: this.api_url,
-			headers: {
-				"Authorization": "Token token="+this.api_token
-			}
-		});
 	}
 
-	test() {
-		this.request.get('v1/users',(error, response) => {
-			console.log(error);
-			console.log(response);
-		});
+	_get(command){
+		return superagent.get(this.api_url+command).set("Authorization", "Token token="+this.api_token).then(response=>response.body)
+	}
+
+	_post(command, data){
+		return superagent.post(this.api_url+command).set("Authorization", "Token token="+this.api_token).send(data).then(response=>response.body)
+	}
+
+	createTicket(ticket){
+		return this._post('v1/tickets', ticket)
+	}
+
+	listTickets() {
+		return this._get('v1/tickets')
+	}
+
+	createUser(user){
+		return this._post('v1/users', user)
+	}
+
+
+	listUsers() {
+		return this._get('v1/users')
 	}
 }
 
