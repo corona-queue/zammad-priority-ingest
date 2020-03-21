0c50395ee86f925c52880a3540bc768244e4dfe9
Marvin Gaube <dev@marvingaube.de>
2020-03-21 17:13:45 +0100

    Fix dynamic port configuration

--- index.js
+++ index.js
@@ -47,6 +47,6 @@ app.post('/ticket', async (req, res) => {
 	}
 })
 
-app.listen(process.env.port || 3000, () => {
+app.listen(process.env.PORT || 3000, () => {
 	console.debug('App listening on :3000');
 });
